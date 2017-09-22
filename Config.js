b804f78bd8fcc16e2d696838ff05867eebf0733c
zumac <zzumac@gmail.com>
2017-09-22 01:56:58 -0400

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,20 @@
+exports.properties = {
+  baseURL : 'https://developer.api.yodlee.com/ysl/restserver/v1/',
+ cobrandParam : {
+    "cobrand":      {
+      "cobrandLogin": "sbCobXXX",
+      "cobrandPassword": "XXX",
+      "locale": "en_US"
+     }
+},
+	userParam: {
+    "user":      {
+      "loginName": "sbMemXXX",
+      "password": "sbMemXXX#123",
+      "locale": "en_US"
+     }
+},
+
+		
+}
+		
